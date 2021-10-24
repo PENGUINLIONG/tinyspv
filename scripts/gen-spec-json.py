1b0393aa10340fb8e2cf1cc6014c4a850be65c3d
PENGUINLIONG <admin@penguinliong.moe>
2021-10-24 18:44:40 +0800

    Parse instructions from HTML

--- scripts/gen-spec-json.py
+++ scripts/gen-spec-json.py
@@ -4,32 +4,23 @@ Generate machine readable JSON referring to the SPIR-V specification.
 """
 from html.parser import HTMLParser
 import json
+from collections import defaultdict
 
 with open("specs/unified1/SPIRV.html") as f:
     spec = f.read()
 
-def decompose_headline(headline: str):
-    parts =  headline.split('.', 4)
-    npart = len(parts)
-    if npart == 2:
-        return (parts[0], None, None, parts[-1])
-    elif npart == 3:
-        return (parts[0], parts[1], None, parts[-1])
-    elif npart == 4:
-        return (parts[0], parts[1], parts[2], parts[-1])
-    else:
-        print(RuntimeError("unexpected headline format"))
-        return (None, None, None, None)
-
 class HeadlineParser:
-    should_update_cur_sec = False
-    should_update_cur_subsec = False
-    should_update_cur_subsubsec = False
-    cur_sec = ""
-    cur_subsec = ""
-    cur_subsubsec = ""
+    def __init__(self):
+        self.should_update_cur_sec = False
+        self.should_update_cur_subsec = False
+        self.should_update_cur_subsubsec = False
+        self.cur_sec = ""
+        self.cur_subsec = ""
+        self.cur_subsubsec = ""
 
-    gather_text = None
+        self.gather_text = None
+
+
     def handle_starttag(self, tag, attrs):
         if tag == "h2" or tag == "h3" or tag == "h4":
             self.gather_text = ""
@@ -39,10 +30,13 @@ class HeadlineParser:
     def handle_endtag(self, tag):
         if tag == "h2":
             self.cur_sec = self.gather_text.strip()
+            self.cur_subsec = "" # Invalidate lower level of headline.
+            self.cur_subsubsec = "" # Invalidate lower level of headline.
             self.gather_text = None
             print((self.cur_sec))
         elif tag == "h3":
             self.cur_subsec = self.gather_text.strip()
+            self.cur_subsubsec = "" # Invalidate lower level of headline.
             self.gather_text = None
             print(" ", (self.cur_subsec))
         elif tag == "h4":
@@ -52,128 +46,197 @@ class HeadlineParser:
     def get_triplet(self):
         return (self.cur_sec, self.cur_subsec, self.cur_subsubsec)
 
-class OperandKindParser:
-    def __init__(self, headline_parser):
-        self.headline_parser = headline_parser
-        
-        self.should_update_column_defs = False
+class TableParser:
+    def __init__(self):
+        self.should_update_col_defs = False
         self.should_update_rows = False
-        self.column_defs = []
+        self.col_defs = []
         self.rows = []
 
         self.gather_text = ""
 
     def handle_starttag(self, tag, attrs):
         if tag == "thead":
-            self.should_update_column_defs = True
+            self.should_update_col_defs = True
         elif tag == "tbody":
-            # The table used to describe literals must have headers. This deals
-            # with info/warning boxes in the text.
-            self.should_update_rows = len(self.column_defs) > 0
+            self.should_update_rows = True
         elif tag == "tr" and self.should_update_rows:
             self.rows += [[]]
-        elif tag == "td" or tag == "th":
+        elif tag == "td" and self.should_update_rows:
+            colspan = int(attrs["colspan"]) if "colspan" in attrs else 1
+            self.rows[-1] += [""] * (colspan - 1)
+            self.gather_text = ""
+        elif tag == "th" and self.should_update_col_defs:
+            colspan = int(attrs["colspan"]) if "colspan" in attrs else 1
+            self.col_defs += [""] * (colspan - 1)
             self.gather_text = ""
     def handle_data(self, data):
         self.gather_text += data
     def handle_endtag(self, tag):
         if tag == "thead":
-            self.should_update_column_defs = False
+            self.should_update_col_defs = False
         elif tag == "tbody":
             self.should_update_rows = False
-        elif tag == "th" and self.should_update_column_defs:
-            self.column_defs += [self.gather_text.strip()]
+        elif tag == "th" and self.should_update_col_defs:
+            self.col_defs += [self.gather_text.strip()]
         elif tag == "td" and self.should_update_rows:
             self.rows[-1] += [self.gather_text.strip()]
-    def to_struct(self, subsec):
-        out = []
-        ncol_def = len(self.column_defs)
 
-        # Some enumerations are not literals but are referred to by result IDs
-        # so there might be a '<id>' suffix at the end of `subsec`. Be aware of
-        # this, we don't use `endswith` here.
-        assert self.column_defs[0] in subsec, \
-            "subsection headline mismatch with the first column header of the enumeration table"
-        # Keep the order, the checks on `ncol_def` shold be done in descending
-        # order.
-        if ncol_def >= 3:
-            assert self.column_defs[1] == "Extra Operands", \
-                "column 2 must be the extra operands"
-            assert self.column_defs[2] == "Enabling Capabilities", \
-                "column 3 must be the enabling capabilities"
-            for row in self.rows:
-                assert len(row) >= ncol_def
-                # If the extra operand is empty, just remove it.
-                if len(row[2]) == 0:
-                    del row[2]
-                elem = {
-                    self.column_defs[0]: row[1],
-                    self.column_defs[1]: row[2:-1],
-                    self.column_defs[2]: row[-1],
-                    "Value": row[0]
-                }
-                out += [elem]
-        elif ncol_def >= 2:
-            # General cases for other literal number specifications.
-            for row in self.rows:
-                assert len(row) == ncol_def + 1
-                elem = dict(zip(["Value"] + self.column_defs, row))
-                out += [elem]
-        elif ncol_def >= 1:
-            for row in self.rows:
-                offset = len(row) - ncol_def
-                assert offset <= 1
-                elem = dict(zip(self.column_defs, row[offset:]))
-                if offset == 1:
-                    # Special case of 'Magic Number'.
-                    elem["Value"] = row[0]
-                out += [elem]
-        else:
-            raise RuntimeError("unsupported column pattern")
-        return out
+def table2enum(table: TableParser, subsec):
+    out = []
+    ncol_def = len(table.col_defs)
+
+    # Some enumerations are not literals but are referred to by result IDs
+    # so there might be a '<id>' suffix at the end of `subsec`. Be aware of
+    # this, we don't use `endswith` here.
+    assert table.col_defs[0] in subsec, \
+        "subsection headline mismatch with the first column header of the enumeration table"
+    # Keep the order, the checks on `ncol_def` shold be done in descending
+    # order.
+    if ncol_def >= 3:
+        assert table.col_defs[1] == "Extra Operands", \
+            "column 2 must be the extra operands"
+        assert table.col_defs[2] == "Enabling Capabilities", \
+            "column 3 must be the enabling capabilities"
+        for row in table.rows:
+            assert len(row) >= ncol_def
+            # If the extra operand is empty, just remove it.
+            if len(row[2]) == 0:
+                del row[2]
+            elem = {
+                table.col_defs[0]: row[1],
+                table.col_defs[1]: row[2:-1],
+                table.col_defs[2]: row[-1],
+                "Value": row[0]
+            }
+            out += [elem]
+    elif ncol_def >= 2:
+        # General cases for other literal number specifications.
+        for row in table.rows:
+            assert len(row) == ncol_def + 1
+            elem = dict(zip(["Value"] + table.col_defs, row))
+            out += [elem]
+    elif ncol_def >= 1:
+        for row in table.rows:
+            offset = len(row) - ncol_def
+            assert offset <= 1
+            elem = dict(zip(table.col_defs, row[offset:]))
+            if offset == 1:
+                # Special case of 'Magic Number'.
+                elem["Value"] = row[0]
+            out += [elem]
+    else:
+        raise RuntimeError("unsupported column pattern")
+    return out
+
+def table2instr(table: TableParser, subsubsec):
+    assert len(table.rows) == 2, \
+        "instruction specification must have two rows"
+
+    first_row = table.rows[0]
+    name, desc = tuple(x.strip() for x in first_row[0].split("\n", maxsplit=1))
+    cap = first_row[1] if len(first_row) > 1 else ""
+
+    second_row = table.rows[1]
+    word_count = second_row[0]
+    opcode = second_row[1]
+    operands = second_row[2:]
+
+    return {
+        "Name": name,
+        "Description": desc,
+        "Enabling Capabilities": cap,
+        "Word Count": word_count,
+        "Opcode": opcode,
+        "Operands": operands,
+    }
+
 
 ENUMERATIONS = {}
+INSTRUCTIONS = defaultdict(list)
 class SpirvSpecParser(HTMLParser):
     def __init__(self):
         super().__init__()
         self.headline_parser = HeadlineParser()
-        self.operand_kind_parser = None
+        self.enum_parser = None
+        self.instr_parser = None
     def handle_starttag(self, tag, attrs):
         self.headline_parser.handle_starttag(tag, attrs)
-        if self.operand_kind_parser:
-            self.operand_kind_parser.handle_starttag(tag, attrs)
+
+        if self.headline_parser.cur_sec.endswith("Binary Form"):
+            if self.headline_parser.cur_subsec.endswith("Instructions"):
+                # Switch to new instruction. Unlike other enumerations,
+                # instruction are grouped in instruction classes and the
+                # instructions are not led by headlines.
+                # We also check whether `last_subsubsec` is empty because there
+                # is a sample `table` above any of the `h4`s and `table`s
+                # which are real instructions specifications.
+                if tag == "table" and len(self.headline_parser.cur_subsubsec) > 0:
+                    # Ignore info boxes which are implemented as `table`s.
+                    attrs = dict(attrs)
+                    if "class" in attrs and "tableblock" in attrs["class"]:
+                        if self.instr_parser == None:
+                            self.instr_parser = TableParser()
+            else:
+                if tag == "table" and len(self.headline_parser.cur_subsec) > 0:
+                    # Ignore info boxes which are implemented as `table`s.
+                    attrs = dict(attrs)
+                    if "class" in attrs and "tableblock" in attrs["class"]:
+                        if self.enum_parser == None:
+                            self.enum_parser = TableParser()
+
+        if self.enum_parser:
+            self.enum_parser.handle_starttag(tag, attrs)
+        if self.instr_parser:
+            self.instr_parser.handle_starttag(tag, attrs)
     def handle_data(self, data):
         self.headline_parser.handle_data(data)
-        if self.operand_kind_parser:
-            self.operand_kind_parser.handle_data(data)
+        if self.enum_parser:
+            self.enum_parser.handle_data(data)
+        if self.instr_parser:
+            self.instr_parser.handle_data(data)
     def handle_endtag(self, tag):
-        global ENUMERATIONS
+        global ENUMERATIONS, INSTRUCTIONS
 
+        last_sec = self.headline_parser.cur_sec
         last_subsec = self.headline_parser.cur_subsec
+        last_subsubsec = self.headline_parser.cur_subsubsec
         self.headline_parser.handle_endtag(tag)
-        cur_sec = self.headline_parser.cur_sec
-        cur_subsec = self.headline_parser.cur_subsec
 
-        if self.operand_kind_parser:
-            self.operand_kind_parser.handle_endtag(tag)
+        if self.enum_parser:
+            self.enum_parser.handle_endtag(tag)
+        if self.instr_parser:
+            self.instr_parser.handle_endtag(tag)
 
-        # Check if we are entering a new subsection.
-        if last_subsec != cur_subsec:
-            # First archive the table we collected and drop the parser.
-            if self.operand_kind_parser != None:
-                ENUMERATIONS[last_subsec.split(' ', maxsplit=2)[1]] = self.operand_kind_parser.to_struct(last_subsec)
-                self.operand_kind_parser = None
-            if cur_sec and cur_sec.endswith("Binary Form"):
-                if cur_subsec.endswith("Instructions"):
-                    pass
-                else:
-                    self.operand_kind_parser = OperandKindParser(self.headline_parser)
+        if last_sec.endswith("Binary Form"):
+            if last_subsec.endswith("Instructions"):
+                # Switch to new instruction. Unlike other enumerations,
+                # instruction are grouped in instruction classes and the
+                # instructions are not led by headlines.
+                # We also check whether `last_subsubsec` is empty because there
+                # is a sample `table` above any of the `h4`s and `table`s
+                # which are real instructions specifications.
+                if tag == "table" and len(last_subsubsec) > 0:
+                    if self.instr_parser != None:
+                        key = last_subsubsec.split(' ', maxsplit=1)[1]
+                        INSTRUCTIONS[key] += [table2instr(self.instr_parser, last_subsubsec)]
+                        self.instr_parser = None
+            else:
+                # Switch to new enumeration.
+                if tag == "table" and len(last_subsec) > 0:
+                    # We only keep the last table.
+                    if self.enum_parser != None:
+                        key = last_subsec.split(' ', maxsplit=1)[1]
+                        ENUMERATIONS[key] = table2enum(self.enum_parser, last_subsec)
+                        self.enum_parser = None
 
 SpirvSpecParser().feed(spec)
 
 out_json = {
-    "enumerations": ENUMERATIONS,
+    "Enumerations": ENUMERATIONS,
+    "Instructions": INSTRUCTIONS,
 }
 
 with open("specs/unified1/SPIRV.json", "w") as f:
     json.dump(out_json, f, indent=4)
+    f.write("\n")
