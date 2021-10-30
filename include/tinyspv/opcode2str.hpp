fef0b9ba5328d998146026a4d247c7533aad0e72
PENGUINLIONG <admin@penguinliong.moe>
2021-10-30 20:15:32 +0800

    Port `opcode2str`

--- include/tinyspv/opcode2str.hpp
+++ include/tinyspv/opcode2str.hpp
@@ -1,4 +1,6 @@
 // THIS IS A GENERATED SOURCE. MODIFICATION WILL BE OVERWRITTEN.
+// USING JSON FROM:
+//   PENGUINLIONG/tinyspv @ c43e82bfb70d813a08d7d10bb8a7a702a0798101
 #pragma once
 #include "spirv/unified1/spirv.hpp"
 namespace tinyspv {
